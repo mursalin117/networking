9343f3651112e2b70be9748f5e51a02b9d7de891
mursalin117 <mursalinchowdhury117@gmail.com>
2021-12-19 00:46:37 +0600

    task for the lab

---
+++ coding/lab work/client/Client.java
@@ -0,0 +1,52 @@
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+  public static Socket s;
+  public static DataInputStream din;
+  public static DataOutputStream dout;
+
+  public static void main(String[] args) {
+    try {
+      s = new Socket("localhost", 6666);
+      din = new DataInputStream(s.getInputStream());
+      dout = new DataOutputStream(s.getOutputStream());
+      String str;
+      Scanner sc = new Scanner(System.in);
+      while (true) {
+        System.out.print("message: ");
+        str = sc.nextLine();
+        dout.writeUTF(str);
+        dout.flush();
+        if (str.equals("stop")) {
+          break;
+        } else if (str.equals("ls")) {
+          getListFiles();
+        }
+      }
+      dout.close();
+      s.close();
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  public static void getListFiles() {
+    try {
+      System.out.println("                           SERVER");
+      while (true){
+        String temp = (String) din.readUTF();
+        System.out.println(temp);
+        if (temp.equals(" ")) {
+          break;
+        }
+      }
+      System.out.println("                             End");
+    } catch(Exception e) {
+      System.out.println("Something went wrong...\n" + e);
+    }
+  }
+}
---
+++ coding/lab work/server/Server.java
@@ -0,0 +1,60 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+  public static ServerSocket ss;
+  public static Socket s;
+  public static DataInputStream dis;
+  public static DataOutputStream dout;
+  public static void main(String[] args) {
+    try {
+      ss = new ServerSocket(6666);
+      s = ss.accept();
+      System.out.println("Server is connected!");
+      dis = new DataInputStream(s.getInputStream());
+      dout = new DataOutputStream(s.getOutputStream());
+      String str;
+      while (true) {
+        str = (String) dis.readUTF();
+        System.out.println("Client: " + str);
+        if (str.equals("stop")) {
+          break;
+        } else if (str.equals("ls")) {
+          File currDir = new File(".");
+          getListFiles(currDir);
+          dout.writeUTF(" ");
+          dout.flush();
+        }
+      }
+      System.out.println("Client is disconnected...\nServer is closing...");
+      dout.close();
+      ss.close();
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  public static void getListFiles(File curDir) {
+    try {
+      File[] filesList = curDir.listFiles();
+      for (File file : filesList) {
+        if (file.isDirectory()) {
+          //System.out.println(file.getPath() + "/");
+          dout.writeUTF(file.getPath() + "/");
+          dout.flush();
+          getListFiles(file);
+        }
+        if (file.isFile()) {
+          //System.out.println(file.getPath() + "----> " + file.getName());
+          dout.writeUTF(file.getPath() + "--> " + file.getName());
+          dout.flush();
+        }
+      }
+    } catch(Exception e) {
+      System.out.println("Somethng went wrong...\n" + e);
+    }
+  }
+}
